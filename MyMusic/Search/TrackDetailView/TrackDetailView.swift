9f3bb6c2d19369433ae5e8810e6fd4e422e08eb2
MarinaKhorunzhina <101120283+MarinaKhorunzhina@users.noreply.github.com>
2022-08-31 21:59:47 +0300

    Track Detail Controller

---
+++ MyMusic/Search/TrackDetailView/TrackDetailView.swift
@@ -0,0 +1,17 @@
+//
+//  TrackDetailView.swift
+//  MyMusic
+//
+//  Created by Marina on 31.08.22.
+//
+
+import UIKit
+
+
+class TrackDetailView: UIView {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+}
