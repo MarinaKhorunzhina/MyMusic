954a949151a261aaa218e4770ebd691a59a71534
MarinaKhorunzhina <101120283+MarinaKhorunzhina@users.noreply.github.com>
2022-09-02 19:37:02 +0300

    Play Tracks with AVPlayer

--- MyMusic/Search/SearchViewController.swift
+++ MyMusic/Search/SearchViewController.swift
@@ -109,6 +109,7 @@ extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
         .first?.windows
         .filter({$0.isKeyWindow}).first
         let trackDetailsView = Bundle.main.loadNibNamed("TrackDetailView", owner: self, options: nil)?.first as! TrackDetailView
+        trackDetailsView.set(viewModel: cellViewModel)
         window?.addSubview(trackDetailsView)
     }
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
--- MyMusic/Search/TrackDetailView/TrackDetailView.swift
+++ MyMusic/Search/TrackDetailView/TrackDetailView.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import SDWebImage
+import AVKit
 
 
 class TrackDetailView: UIView {
@@ -22,9 +24,34 @@ class TrackDetailView: UIView {
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        trackImageView.backgroundColor = .red
+        trackImageView.backgroundColor = .yellow
     }
     
+    let player: AVPlayer = {
+       let avPlayer = AVPlayer()
+        //отменяет задержку в начале проигрывания
+        avPlayer.automaticallyWaitsToMinimizeStalling = false
+        return avPlayer
+    }()
+    
+    func set(viewModel: SearchViewModel.Cell) {
+        trackTitleLabel.text = viewModel.trackName
+        authorTitleLabel.text = viewModel.artistName
+        playTrack(previewUrl: viewModel.previewUrl)
+     //   playTrack(previewUrl: viewModel.previewUrl)
+        let string600 = viewModel.iconUrlString?.replacingOccurrences(of: "100x100", with: "600x600")
+        guard let url = URL(string: string600 ?? "") else { return }
+        trackImageView.sd_setImage(with: url, completed: nil)
+    }
+    
+    private func playTrack(previewUrl: String?) {
+        print("Пытаюсь включить трек по ссылке: \(previewUrl ?? "Отсутствует")")
+        
+        guard let url = URL(string: previewUrl ?? "") else { return }
+        let playerItem = AVPlayerItem(url: url)
+        player.replaceCurrentItem(with: playerItem)
+        player.play()
+    }
     
     @IBAction func handleCurrentTimeSlider(_ sender: Any) {
     }
@@ -43,6 +70,13 @@ class TrackDetailView: UIView {
     }
     
     @IBAction func playPauseAction(_ sender: Any) {
+        if player.timeControlStatus == .paused {
+            player.play()
+            playPauseButton.setImage(#imageLiteral(resourceName: "pause"), for: .normal)
+        } else {
+            player.pause()
+            playPauseButton.setImage(#imageLiteral(resourceName: "play"), for: .normal)
+        }
     }
     
 }
