e761d5d7064b2953c9746df6acdddb9bb9731133
MarinaKhorunzhina <101120283+MarinaKhorunzhina@users.noreply.github.com>
2022-09-14 20:15:12 +0300

    SwiftUI Gestures and Custom Buttons

--- MyMusic/Library.swift
+++ MyMusic/Library.swift
@@ -10,7 +10,11 @@ import URLImage
 
 struct Library: View {
     
-    var tracks = UserDefaults.standard.savedTracks()
+    @State var tracks = UserDefaults.standard.savedTracks()
+    @State private var showingAlert = false
+    @State private var track: SearchViewModel.Cell!
+    
+    var tabBarDelegate: MainTabBarControllerDelegate?
     
     var body: some View {
         NavigationView {
@@ -18,45 +22,83 @@ struct Library: View {
                 GeometryReader { geometry in
                     HStack(spacing: 20) {
                         Button(action: {
-                            print("12345")
+                            self.track = self.tracks[0]
+                            self.tabBarDelegate?.maximizeTrackDetailController(viewModel: self.track)
                         }, label: {
                             Image(systemName: "play.fill")
-                                .frame(width: 180, height: 50)
+                                .frame(width: 180, height: 40)
                                 .accentColor(Color.red)
                                 .background(Color.gray)
                                 .cornerRadius(10)
                         })
                         Button(action: {
-                            print("54321")
+                            self.tracks = UserDefaults.standard.savedTracks()
                         }, label: {
                             Image(systemName: "arrow.2.circlepath")
-                                .frame(width: 180, height: 50)
-                                .accentColor(Color.init(#colorLiteral(red: 0.9921568627, green: 0.1764705882, blue: 0.3333333333, alpha: 1)))
-                                .background(Color.init(#colorLiteral(red: 0.9531342387, green: 0.9490900636, blue: 0.9562709928, alpha: 1)))
+                                .frame(width: 180, height: 40)
+                                .accentColor(Color.red)
+                                .background(Color.gray)
                                 .cornerRadius(10)
                         })
                     }
                 }.padding().frame(height: 50)
-                Divider().padding(.leading).padding(.trailing).padding(.top)
+                Divider().padding(.leading).padding(.trailing)
                 
-                List(tracks) { track in
-                    LibraryCell(cell: track)
-                   
+                List {
+                    ForEach(tracks) { track in
+                        LibraryCell(cell: track).gesture(
+                            LongPressGesture()
+                                .onEnded { _ in
+                                    self.track = track
+                                    self.showingAlert = true
+                                }
+                                .simultaneously(with: TapGesture()
+                                    .onEnded { _ in
+                                        self.track = track
+                                        self.tabBarDelegate?.maximizeTrackDetailController(viewModel: self.track)
+                                    }))
+                    }
+                    .onDelete(perform: delete)
+                    
                 }
-            }
+            }.actionSheet(isPresented: $showingAlert, content: {
+                ActionSheet(title: Text("Are you sure you want to delete this track?"), buttons: [.destructive(Text("Delete"), action: {
+                    print("Deleting: \(self.track.trackName)")
+                    self.delete(track: self.track)
+                }), .cancel()
+                ])
+            })
                 .navigationBarTitle("Library")
-                
         }
         
+    }
+    
+    func delete(at offsets: IndexSet) {
+        tracks.remove(atOffsets: offsets)
+        if let savedData = try? NSKeyedArchiver.archivedData(withRootObject: tracks, requiringSecureCoding: false) {
+            let defaults = UserDefaults.standard
+            defaults.set(savedData, forKey: UserDefaults.favouriteTrackKey)
+        }
+    }
+    
+    func delete(track: SearchViewModel.Cell) {
+        let index = tracks.firstIndex(of: track)
+        guard let myIndex = index else { return }
+        tracks.remove(at: myIndex)
+        if let savedData = try? NSKeyedArchiver.archivedData(withRootObject: tracks, requiringSecureCoding: false) {
+            let defaults = UserDefaults.standard
+            defaults.set(savedData, forKey: UserDefaults.favouriteTrackKey)
+        }
     }
 }
 
 struct LibraryCell: View {
+    
     var cell: SearchViewModel.Cell
     
     var body: some View {
         HStack {
-            URLImage(URL(string: cell.iconUrlString ?? "")!).resizable().frame(width: 60, height: 60).cornerRadius(2)
+           // URLImage(URL(string: cell.iconUrlString ?? "")!).resizable().frame(width: 60, height: 60).cornerRadius(2)
             VStack(alignment: .leading) {
                 Text("\(cell.trackName)")
                 Text("\(cell.artistName)")
@@ -71,4 +113,3 @@ struct Library_Previews: PreviewProvider {
         Library()
     }
 }
-
